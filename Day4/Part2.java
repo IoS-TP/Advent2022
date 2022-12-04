77690ce7db15bc7a0edc4921eecb17445db809a7
mdb326 <mdb326@lehigh.edu>
2022-12-04 10:23:34 -0500

    Part2 done

---
+++ Day4/Part2.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Part2 {
+    public static int total = 0;
+    public static void main(String[] args){
+        ArrayList<String[]> list = new ArrayList<>();
+        readFile(list);
+        for (String[] line : list){
+            comparer(line[0], line[1]);
+        }
+        System.out.println(total);
+
+    }
+    public static void readFile(ArrayList<String[]> list){
+        File file = new File("input.txt");
+        try{
+            Scanner reader = new Scanner(file);
+            while(reader.hasNextLine()){
+                String line = reader.nextLine();
+                list.add(line.split(","));
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Cant find file");
+            System.exit(0);
+        }
+    }
+    public static void comparer(String one, String two){    
+        String[] first = one.split("-");
+        String[] second = two.split("-");
+
+        if(Integer.parseInt(first[0]) >= Integer.parseInt(second[0]) && Integer.parseInt(first[0]) <= Integer.parseInt(second[1])){
+            total++;
+        }
+        else if(Integer.parseInt(first[1]) >= Integer.parseInt(second[0])&& Integer.parseInt(first[1]) <= Integer.parseInt(second[1])){
+            total++;
+        }
+        else if(Integer.parseInt(second[0]) >= Integer.parseInt(first[0]) && Integer.parseInt(second[0]) <= Integer.parseInt(first[1])){
+            total++;
+        }
+        else if(Integer.parseInt(second[0]) >= Integer.parseInt(first[0]) && Integer.parseInt(second[0]) <= Integer.parseInt(first[1])){
+            total++;
+        }
+    }   
+}
